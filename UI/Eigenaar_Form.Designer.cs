fd1b32decfe60e5cacf39d87add2bb9382936657
giannistek1 <gianni.stekkinger@hotmail.nl>
2017-06-11 00:25:44 +0200

    Voorraad display verwerkt, medewerkers alleen de top kolommen, menukaarten leeg, Toevoegen, wijzigen, verwijderen knoppen toegevoegd (niet functioneel)

--- UI/Eigenaar_Form.Designer.cs
+++ UI/Eigenaar_Form.Designer.cs
@@ -30,32 +30,79 @@
         {
             this.lblTitel = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listViewOverzicht = new System.Windows.Forms.ListView();
             this.btnVoorraad = new System.Windows.Forms.Button();
             this.btnMedewerkers = new System.Windows.Forms.Button();
-            this.btnMenuItems = new System.Windows.Forms.Button();
+            this.btnMenuKaarten = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitel
             // 
             this.lblTitel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitel.Location = new System.Drawing.Point(300, 122);
+            this.lblTitel.Location = new System.Drawing.Point(302, 132);
             this.lblTitel.Name = "lblTitel";
             this.lblTitel.Size = new System.Drawing.Size(150, 25);
             this.lblTitel.TabIndex = 7;
-            this.lblTitel.Text = "Voorraad";
+            this.lblTitel.Text = "Overzicht";
             this.lblTitel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.listViewOverzicht);
             this.panel2.Location = new System.Drawing.Point(22, 160);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(694, 569);
             this.panel2.TabIndex = 6;
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(513, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 107);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Verwijderen";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(513, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 107);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Wijzigen";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(513, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 107);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Toevoegen";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listViewOverzicht
+            // 
+            this.listViewOverzicht.Location = new System.Drawing.Point(3, 3);
+            this.listViewOverzicht.Name = "listViewOverzicht";
+            this.listViewOverzicht.Size = new System.Drawing.Size(505, 563);
+            this.listViewOverzicht.TabIndex = 0;
+            this.listViewOverzicht.UseCompatibleStateImageBehavior = false;
+            // 
             // btnVoorraad
             // 
             this.btnVoorraad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVoorraad.Location = new System.Drawing.Point(22, 70);
+            this.btnVoorraad.Location = new System.Drawing.Point(25, 70);
             this.btnVoorraad.Name = "btnVoorraad";
             this.btnVoorraad.Size = new System.Drawing.Size(174, 68);
             this.btnVoorraad.TabIndex = 8;
@@ -68,28 +115,28 @@
             this.btnMedewerkers.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMedewerkers.Location = new System.Drawing.Point(287, 70);
             this.btnMedewerkers.Name = "btnMedewerkers";
-            this.btnMedewerkers.Size = new System.Drawing.Size(178, 38);
+            this.btnMedewerkers.Size = new System.Drawing.Size(178, 59);
             this.btnMedewerkers.TabIndex = 9;
             this.btnMedewerkers.Text = "Medewerkers";
             this.btnMedewerkers.UseVisualStyleBackColor = true;
             this.btnMedewerkers.Click += new System.EventHandler(this.btnMedewerkers_Click);
             // 
-            // btnMenuItems
+            // btnMenuKaarten
             // 
-            this.btnMenuItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMenuItems.Location = new System.Drawing.Point(538, 70);
-            this.btnMenuItems.Name = "btnMenuItems";
-            this.btnMenuItems.Size = new System.Drawing.Size(178, 68);
-            this.btnMenuItems.TabIndex = 10;
-            this.btnMenuItems.Text = "Menukaarten";
-            this.btnMenuItems.UseVisualStyleBackColor = true;
-            this.btnMenuItems.Click += new System.EventHandler(this.btnMenuItems_Click);
+            this.btnMenuKaarten.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenuKaarten.Location = new System.Drawing.Point(535, 70);
+            this.btnMenuKaarten.Name = "btnMenuKaarten";
+            this.btnMenuKaarten.Size = new System.Drawing.Size(178, 68);
+            this.btnMenuKaarten.TabIndex = 10;
+            this.btnMenuKaarten.Text = "Menukaarten";
+            this.btnMenuKaarten.UseVisualStyleBackColor = true;
+            this.btnMenuKaarten.Click += new System.EventHandler(this.btnMenuKaarten_Click);
             // 
             // Eigenaar_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(752, 741);
-            this.Controls.Add(this.btnMenuItems);
+            this.Controls.Add(this.btnMenuKaarten);
             this.Controls.Add(this.btnMedewerkers);
             this.Controls.Add(this.btnVoorraad);
             this.Controls.Add(this.lblTitel);
@@ -100,7 +147,8 @@
             this.Controls.SetChildIndex(this.lblTitel, 0);
             this.Controls.SetChildIndex(this.btnVoorraad, 0);
             this.Controls.SetChildIndex(this.btnMedewerkers, 0);
-            this.Controls.SetChildIndex(this.btnMenuItems, 0);
+            this.Controls.SetChildIndex(this.btnMenuKaarten, 0);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -111,6 +159,10 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnVoorraad;
         private System.Windows.Forms.Button btnMedewerkers;
-        private System.Windows.Forms.Button btnMenuItems;
+        private System.Windows.Forms.Button btnMenuKaarten;
+        private System.Windows.Forms.ListView listViewOverzicht;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
--- UI/Eigenaar_Form.cs
+++ UI/Eigenaar_Form.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Logica;
+using Model;
 
 namespace UI
 {
@@ -14,34 +16,71 @@ namespace UI
         {
             InitializeComponent();
         }
-        private void Voorraad_Form_Load()
-        {
 
+        private void InitListView()
+        {
+            ListView listViewOverzicht = new ListView();
+            listViewOverzicht.Height = 563;
+            listViewOverzicht.Width = 688;
         }
 
         private void btnVoorraad_Click(object sender, EventArgs e)
         {
-            panel2.Controls.Clear();
+            listViewOverzicht.Clear();
             lblTitel.Text = "Voorraad";
 
-            /*Button btnToevoegen = new Button();
-            btnToevoegen.Text = "... Toevoegen";
-            btnToevoegen.Location = new Point(420, 0);
-            btnToevoegen.AutoSize = true;
+            InitListView();
 
-            panel2.Controls.Add(btnToevoegen);*/
+            List<Model.MenuItem> MI_lijst = new List<Model.MenuItem>(); //maak list aan
+            MenuItems menuItems = new MenuItems(); //maak object aan
+            MI_lijst = menuItems.make_list(); //vul lijst
+
+            listViewOverzicht.View = View.Details;
+            listViewOverzicht.Columns.Add("Id", 50);
+            listViewOverzicht.Columns.Add("Naam", 300);
+            listViewOverzicht.Columns.Add("Voorraad", 100);
+
+            int aantal = MI_lijst.Count;
+
+            for (int i = 0; i < aantal; i++)
+            {
+                ListViewItem li = new ListViewItem(MI_lijst[i].id.ToString());
+                li.SubItems.Add(MI_lijst[i].naam.ToString());
+                li.SubItems.Add(MI_lijst[i].voorraad.ToString());
+
+                listViewOverzicht.Items.Add(li);
+            }
+            //panel2.Controls.Add(listViewOverzicht);
         }
 
         private void btnMedewerkers_Click(object sender, EventArgs e)
         {
-            panel2.Controls.Clear();
+            listViewOverzicht.Clear();
             lblTitel.Text = "Medewerkers";
+
+            InitListView();
+
+            List<Werknemer> w_lijst = new List<Werknemer>();
+
+            listViewOverzicht.View = View.Details;
+            listViewOverzicht.Columns.Add("Id", 50);
+            listViewOverzicht.Columns.Add("Voornaam", 150);
+            listViewOverzicht.Columns.Add("Achternaam", 150);
+            listViewOverzicht.Columns.Add("Functie", 100);
+
         }
 
-        private void btnMenuItems_Click(object sender, EventArgs e)
+        private void btnMenuKaarten_Click(object sender, EventArgs e)
         {
-            panel2.Controls.Clear();
+            listViewOverzicht.Clear();
             lblTitel.Text = "Menukaarten";
+
+            InitListView();
+
+            List<Model.MenuItem> MI_lijst = new List<Model.MenuItem>(); //maak list aan
+            MenuItems menuItems = new MenuItems(); //maak object aan
+            MI_lijst = menuItems.make_list(); //vul lijst
+
         }
     }
 }
