0cce5781b937a3a6ef65b03db13b486b14bc7ff3
urvo <585521@student.inholland.nl>
2017-06-14 15:16:00 +0200

    nog minder errors nu

---
+++ Logica/Werknemer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Model;
+
+namespace Logica
+{
+    public class Werknemer
+    {
+
+
+        public List<Model.Werknemer> GetAll()
+        {
+            DALWerknemer dalwerknemer = new DALWerknemer();
+            return dalwerknemer.GetAll();
+        }
+
+
+        //  public string check_werknemer (List<Werknemer>werknemers, string invoer_wachtwoord)
+        // {
+        //      string naam_werknemer = "";
+        //      DALLogin loginCheck = new DALLogin ();
+        //      DALWerknemer werknemer = new DALWerknemer();
+        //      if (int.Parse(invoer_wachtwoord) == loginCheck.GetPassword(invoer_wachtwoord))
+        //      {
+
+        // naam_werkgever = werknemer.werknemers[0].naam;  
+
+        //          return naam_werkgever;
+        //     }
+
+        //     else
+        //     {
+        //        naam_werkgever = "";
+        //        return naam_werkgever;
+        //    }
+        // }
+    }
+}
